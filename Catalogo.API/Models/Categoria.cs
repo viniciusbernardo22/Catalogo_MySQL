93df98f5c9bac033f5a66b0c2eeabf3b1d86668f
Vinicius <vini383@gmail.com>
2024-05-07 14:33:59 -0300

    feat: init ASP.NET Core project and added dependencies

---
+++ Catalogo.API/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalogo.API.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
